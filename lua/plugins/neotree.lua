c9d6b294da15ee5f4ebde0cfdb42bafec4c14156
Mykola Yakovliev <mykola.yakovliev@outlook.com>
2025-08-30 09:42:08 +0200

    Close file explorer on file open

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,17 +1,26 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle<CR>")
-    end,
-  }
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons", -- optional, but recommended
+  },
+  lazy = false, -- neo-tree will lazily load itself
+  config = function()
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle<CR>")
+    require("neo-tree").setup({
+      event_handlers = {
+        {
+          event = "file_open_requested",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end
+        },
+  
+      }
+    })
+  end,
 }
 
