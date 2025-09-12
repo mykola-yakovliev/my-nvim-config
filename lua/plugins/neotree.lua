d3f10a3c5c35d07ab2c71c496064dfbe666276b6
Mykola Yakovliev <29441643+mykola-yakovliev@users.noreply.github.com>
2025-09-12 16:09:32 +0200

    Update neotree.lua

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,24 +1,30 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons", -- optional, but recommended
-  },
-  lazy = false, -- neo-tree will lazily load itself
-  config = function()
-    vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle<CR>")
-    require("neo-tree").setup({
-      event_handlers = {
-        {
-          event = "file_open_requested",
-          handler = function()
-            require("neo-tree.command").execute({ action = "close" })
-          end
-        },
-      }
-    })
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	lazy = false,
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle<CR>")
+		require("neo-tree").setup({
+			filesystem = {
+				follow_current_file = {
+					enabled = true,
+					leave_dirs_open = false,
+				},
+				hijack_netrw_behavior = "open_current",
+			},
+			event_handlers = {
+				{
+					event = "file_open_requested",
+					handler = function()
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		})
+	end,
 }
-
