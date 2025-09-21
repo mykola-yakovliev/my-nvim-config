a9618ef8375bd336aaea720d936d9142ab6c3551
Mykola Yakovliev <mykola.yakovliev@outlook.com>
2025-09-21 10:35:43 +0200

    Migrate to newer LSP config

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -45,9 +45,7 @@ return {
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      local lspconfig = require("lspconfig")
-
-      lspconfig.lua_ls.setup({
+      vim.lsp.config('*', {
         capabilities = capabilities
       })
     end,
