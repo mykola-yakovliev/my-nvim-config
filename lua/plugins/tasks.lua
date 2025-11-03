971d4c22a9e6fc9a89995446f0a48ee38923fe06
Mykola Yakovliev <29441643+mykola-yakovliev@users.noreply.github.com>
2025-11-03 07:56:12 +0100

    Update tasks.lua - Overseer Quick Action

--- lua/plugins/tasks.lua
+++ lua/plugins/tasks.lua
@@ -1,9 +1,15 @@
 return {
-  "stevearc/overseer.nvim",
-  opts = {},
-  config = function()
-    require("overseer").setup()
-    vim.keymap.set("n", "<leader>ot", ":OverseerToggle<CR>")
-    vim.keymap.set("n", "<leader>or", ":OverseerRun<CR>")
-  end
+	"stevearc/overseer.nvim",
+	opts = {},
+	config = function()
+		require("overseer").setup()
+		vim.keymap.set("n", "<leader>ot", ":OverseerToggle<CR>")
+		vim.keymap.set("n", "<leader>or", ":OverseerRun<CR>")
+		vim.keymap.set(
+			"n",
+			"<leader>oq",
+			"<Cmd>OverseerQuickAction open float<CR>",
+			{ desc = "Overseer: open last output" }
+		)
+	end,
 }
