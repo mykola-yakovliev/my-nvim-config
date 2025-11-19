843f40cae17770929ab14a8edabc41f2b9a31895
Mykola Yakovliev <mykola.yakovliev@outlook.com>
2025-11-19 22:33:19 +0100

    Pin overseer at v1.6

--- lua/plugins/tasks.lua
+++ lua/plugins/tasks.lua
@@ -1,15 +1,16 @@
 return {
-	"stevearc/overseer.nvim",
-	opts = {},
-	config = function()
-		require("overseer").setup()
-		vim.keymap.set("n", "<leader>ot", ":OverseerToggle<CR>")
-		vim.keymap.set("n", "<leader>or", ":OverseerRun<CR>")
-		vim.keymap.set(
-			"n",
-			"<leader>oq",
-			"<Cmd>OverseerQuickAction open float<CR>",
-			{ desc = "Overseer: open last output" }
-		)
-	end,
+  "stevearc/overseer.nvim",
+  version = "1.6",
+  opts = {},
+  config = function()
+    require("overseer").setup()
+    vim.keymap.set("n", "<leader>ot", ":OverseerToggle<CR>")
+    vim.keymap.set("n", "<leader>or", ":OverseerRun<CR>")
+    vim.keymap.set(
+      "n",
+      "<leader>oq",
+      "<Cmd>OverseerQuickAction open float<CR>",
+      { desc = "Overseer: open last output" }
+    )
+  end,
 }
