9d9885012796e356834aead197a5b99df2528e38
Mykola Yakovliev <mykola.yakovliev@outlook.com>
2025-11-30 00:11:21 +0100

    Enable window navigation & create LazyGit shortcut

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -5,5 +5,6 @@ return {
   },
   config = function()
     vim.g.lazygit_floating_window_use_plenary = 1
+    vim.keymap.set("n", "<leader>lg", ':LazyGit<CR>', { noremap = true, silent = true })
   end
 }
