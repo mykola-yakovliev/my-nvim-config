456faa16222e60f79ac7db4bc855c012aba6ea7d
Mykola Yakovliev <mykola.yakovliev@outlook.com>
2025-11-30 01:41:34 +0100

    Configure debugging for easy-dotnet

--- lua/plugins/dotnet.lua
+++ lua/plugins/dotnet.lua
@@ -2,7 +2,17 @@ return {
   "GustavEikaas/easy-dotnet.nvim",
   dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
   config = function()
-    require("easy-dotnet").setup()
+    local nvim_path = vim.fn.stdpath("config")
+    local netcoredbg_path = nvim_path .. "/executables/netcoredbg/netcoredbg.exe"
+
+    local dotnet = require('easy-dotnet')
+    dotnet.setup({
+      debugger = {
+        bin_path = netcoredbg_path,
+      }
+    })
+
+    require("easy-dotnet.netcoredbg").register_dap_variables_viewer()
 
     vim.keymap.set("n", "<leader>Dn", ':Dotnet<CR>', { noremap = true, silent = true })
   end
