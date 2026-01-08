3160df4bd90f107f3567835e4f9cf78718ba579f
mykola-yakovliev <mykola.yakovliev.it@outlook.com>
2026-01-08 19:44:30 +0100

    Go to Implementation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,52 +1,52 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {},
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "lua_ls", "ts_ls", "jsonls", "terraformls", "angularls" },
-    },
-    dependencies = {
-      "mason-org/mason.nvim",
-      "neovim/nvim-lspconfig",
-    },
-    config = function(_, opts)
-      require("mason-lspconfig").setup(opts)
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "lua_ls", "ts_ls", "jsonls", "terraformls", "angularls" },
+		},
+		dependencies = {
+			"mason-org/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		config = function(_, opts)
+			require("mason-lspconfig").setup(opts)
 
-      vim.diagnostic.config({
-        virtual_text = {
-          prefix = "●",
-        },
-        signs = true,
-        underline = true,
-        update_in_insert = false,
-        severity_sort = true,
-      })
+			vim.diagnostic.config({
+				virtual_text = {
+					prefix = "●",
+				},
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
 
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local bufnr = args.buf
-          local bufmap = function(mode, lhs, rhs, desc)
-            vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-          end
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local bufnr = args.buf
+					local bufmap = function(mode, lhs, rhs, desc)
+						vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+					end
 
-          -- Code Actions
-          bufmap("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action")
-          bufmap("v", "<leader>ca", vim.lsp.buf.code_action, "Code Action (Visual)")
+					-- Code Actions
+					bufmap("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action")
+					bufmap("v", "<leader>ca", vim.lsp.buf.code_action, "Code Action (Visual)")
 
-          -- Other useful LSP keymaps
-          bufmap("n", "gd", vim.lsp.buf.definition, "Go to Definition")
-          bufmap("n", "K", vim.lsp.buf.hover, "Hover Documentation")
-          bufmap("n", "<leader>rn", vim.lsp.buf.rename, "Rename Symbol")
-          bufmap("n", "fr", vim.lsp.buf.references, "Find References")
+					-- Other useful LSP keymaps
+					bufmap("n", "gd", vim.lsp.buf.definition, "Go to Definition")
+					bufmap("n", "gi", vim.lsp.buf.implementation, "Go to Implementation")
+					bufmap("n", "K", vim.lsp.buf.hover, "Hover Documentation")
+					bufmap("n", "<leader>rn", vim.lsp.buf.rename, "Rename Symbol")
+					bufmap("n", "fr", vim.lsp.buf.references, "Find References")
 
-          bufmap("n", "[d", vim.diagnostic.goto_prev, "Previous Diagnostic")
-          bufmap("n", "]d", vim.diagnostic.goto_next, "Next Diagnostic")
-        end,
-      })
-    end,
-  },
+					bufmap("n", "[d", vim.diagnostic.goto_prev, "Previous Diagnostic")
+					bufmap("n", "]d", vim.diagnostic.goto_next, "Next Diagnostic")
+				end,
+			})
+		end,
+	},
 }
-
