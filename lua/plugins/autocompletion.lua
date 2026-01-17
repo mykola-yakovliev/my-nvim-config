ccbbf300fcb49df8818e718d36b3b993c4013ab0
Mykola Yakovliev <29441643+mykola-yakovliev@users.noreply.github.com>
2026-01-17 17:58:30 +0100

    Refactor autocompletion configuration in Lua

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -61,15 +61,5 @@ return {
       })
     end,
   },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-      vim.lsp.config('*', {
-        capabilities = capabilities
-      })
-    end,
-  },
 }
+
