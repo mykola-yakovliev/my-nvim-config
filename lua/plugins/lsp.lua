e4f9ee7109579e1fd4ec0bf32c63ef8e77978f90
mykola-yakovliev <mykola.yakovliev.it@outlook.com>
2026-01-23 23:44:33 +0100

    Improve diagnostics window

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -102,4 +102,15 @@ return {
 			})
 		end,
 	},
+	{
+		"rachartier/tiny-inline-diagnostic.nvim",
+		event = "VeryLazy",
+		priority = 1000,
+		config = function()
+			require("tiny-inline-diagnostic").setup({
+        preset = "simple",
+      })
+			vim.diagnostic.config({ virtual_text = false })
+		end,
+	},
 }
