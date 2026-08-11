c0e214b141f79e10778bdd45cf2ff8fd04adbae0
mykola-yakovliev <mykola.yakovliev@outlook.com>
2026-08-11 09:43:54 +0200

    Install maximize window

--- lua/plugins/window.lua
+++ lua/plugins/window.lua
@@ -1,7 +1,26 @@
 return {
-  "yorickpeterse/nvim-window",
-  keys = {
-    { "<leader>wj", "<cmd>lua require('nvim-window').pick()<cr>", desc = "nvim-window: Jump to window" },
-  },
-  config = true,
+	{
+		"yorickpeterse/nvim-window",
+		keys = {
+			{ "<leader>wj", "<cmd>lua require('nvim-window').pick()<cr>", desc = "nvim-window: Jump to window" },
+		},
+		config = true,
+	},
+	{
+		"declancm/maximize.nvim",
+		config = {
+			plugins = {
+				dapui = {
+					enable = true,
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>wm",
+				"<cmd>Maximize<cr>",
+				desc = "Maximize window",
+			},
+		},
+	},
 }
